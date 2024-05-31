df3d3f2ca422942fa5ae81ed4a9cba9cad935d54
Akhrorfayzullo <akhrorfayzullo@gmail.com>
2024-05-31 20:16:51 +0900

    Task E

--- train.js
+++ train.js
@@ -1,14 +1,24 @@
 //taskd
-function checkContent(str1, str2) {
-  if (str1.length !== str2.length) {
-    return false;
-  }
+// function checkContent(str1, str2) {
+//   if (str1.length !== str2.length) {
+//     return false;
+//   }
 
-  const sortedStr1 = str1.split("").sort().join("");
-  const sortedStr2 = str2.split("").sort().join("");
+//   const sortedStr1 = str1.split("").sort().join("");
+//   const sortedStr2 = str2.split("").sort().join("");
 
-  return sortedStr1 === sortedStr2;
+//   return sortedStr1 === sortedStr2;
+// }
+
+// console.log(checkContent("mitgroup", "gmtiprou"));
+// console.log(checkContent("hello", "Bitcpin"));
+
+//Task E
+//MASALAN: getReverse("hello") return qilsin "olleh"
+const word = "hello";
+
+function getReverse(word) {
+  return word.split("").reverse().join("");
 }
 
-console.log(checkContent("mitgroup", "gmtiprou"));
-console.log(checkContent("hello", "Bitcpin"));
+console.log(`${word} so'zi ${getReverse(word)} ga o'zgardi`);
