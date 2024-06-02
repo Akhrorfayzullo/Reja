a19e7154d82c3e6f3558e98ec6d8f7729b83f4c5
Akhrorfayzullo <akhrorfayzullo@gmail.com>
2024-06-02 19:47:01 +0900

    Trainn

--- train.js
+++ train.js
@@ -1,24 +1,18 @@
-//taskd
-// function checkContent(str1, str2) {
-//   if (str1.length !== str2.length) {
-//     return false;
-//   }
+function findDoublers(str) {
+  const charCount = {};
 
-//   const sortedStr1 = str1.split("").sort().join("");
-//   const sortedStr2 = str2.split("").sort().join("");
+  for (let char of str) {
+    if (charCount[char]) {
+      return true;
+    } else {
+      charCount[char] = 1;
+    }
+  }
 
-//   return sortedStr1 === sortedStr2;
-// }
-
-// console.log(checkContent("mitgroup", "gmtiprou"));
-// console.log(checkContent("hello", "Bitcpin"));
-
-//Task E
-//MASALAN: getReverse("hello") return qilsin "olleh"
-const word = "hello";
-
-function getReverse(word) {
-  return word.split("").reverse().join("");
+  return false;
 }
 
-console.log(`${word} so'zi ${getReverse(word)} ga o'zgardi`);
+console.log(findDoublers("hello"));
+console.log(findDoublers("world"));
+console.log(findDoublers("example"));
+console.log(findDoublers("test"));
